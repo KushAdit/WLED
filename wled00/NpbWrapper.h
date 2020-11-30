98d008d91c152ca54cd81923946070626990b3be
KushAdit <adityakushsolan@gmail.com>
2020-11-30 10:33:55 +0000

    BRG Strips

--- wled00/NpbWrapper.h
+++ wled00/NpbWrapper.h
@@ -169,7 +169,7 @@
  #define PIXELFEATURE3 P9813BgrFeature
  #define PIXELFEATURE4 NeoGrbwFeature
 #else
- #define PIXELFEATURE3 NeoGrbFeature
+ #define PIXELFEATURE3 NeoBrgFeature
  #define PIXELFEATURE4 NeoGrbwFeature
 #endif
 
